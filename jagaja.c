cace594ea5d1a4ae436265b9e48ca33f181de81d
Raido Kalbre <RKalbre@gmail.com>
2011-06-10 11:04:24 +0300

    accidentally deleted boolean definitions last time

--- jagaja.c
+++ jagaja.c
@@ -1,6 +1,10 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 
+#define TRUE 1
+#define FALSE 0
+
+
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * *
  *   Suvalise märgiga (täis)arvude vahetu jagamine   *
